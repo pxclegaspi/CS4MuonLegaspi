f76ba4659d07a25f26048b448a4f6a9a92226e8e
pxclegaspi <96472436+pxclegaspi@users.noreply.github.com>
2022-09-06 08:45:48 +0800

    Add files via upload

---
+++ 2025 Mu15 comp sci practice/src/pkg2025/mu15/comp/sci/practice/Mu15CompSciPractice.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2025.mu15.comp.sci.practice;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MUON
+ */
+public class Mu15CompSciPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 3;
+        double decimal = 4.5;
+        char letter = '@';
+        String name = "Legaspi";
+        String want = "copypasta";
+        boolean trueOrFalse = true;
+        
+        System.out.println("Hoy, " + name + "! Namimihasa ka na!");
+        System.out.print(num + "" + decimal + letter + name + trueOrFalse);
+        
+        if(trueOrFalse) {
+            System.out.println("Nice! You're not telling lies.");
+        } else {
+            System.out.println("Why you lie?");
+        }
+        
+        for(int p=0; p<5; p++) {
+            System.out.printf("I love %s! %n", want);
+        }
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String inputExample = sc.nextLine();
+        int result = Integer.parseInt(inputExample);
+    }
+    
+}
