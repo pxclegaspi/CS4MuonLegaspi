35c79abd7555308acc0c3b5e849d42375b65d981
pxclegaspi <96472436+pxclegaspi@users.noreply.github.com>
2022-09-08 11:28:21 +0800

    Add files via upload

--- Ex01-Muon-Legaspi/src/ex01/muon/legaspi/Ex01MuonLegaspi.java
+++ Ex01-Muon-Legaspi/src/ex01/muon/legaspi/Ex01MuonLegaspi.java
@@ -23,6 +23,37 @@ public class Ex01MuonLegaspi {
         String nameTwo = "Spongebob Squarepants";
         int ageTwo = 23;
         int episodesTwo = 250;
+        
+        String nameThree = "Philippine Science High School System";
+        int ageThree = 58;
+        int campusesThree = 16;
+        
+        System.out.printf("Name of Student: %s %n", nameOne);
+        System.out.printf("Age: %s %n", ageOne);
+        System.out.printf("Height: %s cm %n%n", heightOne);
+        
+        System.out.printf("Favorite Show: %s%n", nameTwo);
+        System.out.printf("Number of years airing: %s%n", ageTwo);
+        System.out.printf("Number of episodes: %s%n%n", episodesTwo);
+        
+        System.out.printf("Current school: %s%n", nameThree);
+        System.out.printf("Number of years running: %s%n", ageThree);
+        System.out.printf("Number of campuses: %s%n%n", campusesThree);
+        
+        if(ageThree>ageTwo) {
+            System.out.printf("%s is older than %s.%n", nameThree, nameTwo);
+        } else {
+            System.out.printf("%s is older than %s.%n", nameTwo, nameThree);
+        }
+        
+        while(ageOne < ageTwo) {
+            int counter = 0;
+            if(ageOne + counter == ageTwo) {
+                System.out.printf("%s is as old in %s years as %s is now.%n", nameOne, counter, nameTwo);
+            } else {
+                counter++;
+            }
+        }
     }
     
 }
